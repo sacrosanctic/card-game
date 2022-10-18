d526c9ff5ee6c1f28f637ae46eef8d61a434c48d
Scott Wu <sw@scottwu.ca>
2022-10-19 02:51:14 +0800

    make quotes

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -28,6 +28,9 @@
 			<li class:active={$page.url.pathname === '/url-states'}>
 				<a href="/url-states">url-states</a>
 			</li>
+			<li class:active={$page.url.pathname === '/quotes'}>
+				<a href="/quotes">quotes</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ src/routes/quotes/+page.svelte
@@ -0,0 +1,33 @@
+<script type="ts">
+	let canvas;
+	const quotes = [
+		'Who are the stakeholders\n\nWhat can they gain or lose',
+		'What is the actual solution\n\nAnd who has the power to enact it',
+		'Where can I find that info\n\nand is it worth my time'
+	];
+
+	const render = (canvas: HTMLCanvasElement) => {
+		console.log(canvas);
+		const { width, height } = canvas;
+
+		const ctx = canvas.getContext('2d');
+		const xPos = width / 2;
+		const yPos = height / 2;
+		const fontSize = 120;
+
+		ctx!.fillStyle = '#111111';
+		ctx?.fillRect(0, 0, width, height);
+
+		ctx!.textBaseline = 'middle';
+		ctx!.textAlign = 'center';
+		ctx!.font = `${fontSize}px Arial`;
+		ctx!.fillStyle = '#cccccc';
+		const quote = quotes[1].split('\n');
+
+		quote.forEach((q, i) => {
+			ctx!.fillText(q, xPos, -fontSize + yPos + i * fontSize);
+		});
+	};
+</script>
+
+<canvas use:render id="test" width="1920" height="1080" style="border:1px solid black" />
